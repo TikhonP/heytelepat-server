80ef44b8eb7ae49e2c280688811fa7b988901a15
Tikhon <tikhon.petrishchev@gmail.com>
2021-11-08 17:08:27 +0300

    v0.2.1 #server #firmware #stable: - Добавлена страница issues - Добавлена интеграция со слаком

--- heytelepat/settings/base.py
+++ heytelepat/settings/base.py
@@ -141,3 +141,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+SLACK_TOKEN = 'xoxb-195247247203-2691473125334-RZtG9tX2eK019nhETSrD6tih'
+SLACK_CHANNEL_ID = 'C02LBCRVB46'
