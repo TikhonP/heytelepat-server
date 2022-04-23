005c3dbce7bb65513ebdb427174610f71b623566
TikhonP <tikhon.petrishchev@gmail.com>
2022-04-23 21:01:08 +0300

    Fixed requirements.txt and phone_call_manager.py phones

--- phone_call_manager/phone_call_manager.py
+++ phone_call_manager/phone_call_manager.py
@@ -16,25 +16,25 @@ VOXIMPLANT_SCENARIO_ID = 33816
 VOXIMPLANT_CALLER_ID = '+79581008748'
 
 PATIENTS = [
-    # {'phone': '+79517287194', 'token': '6Hnojq9_fwS-yXaiawJ1og'},
-    # {'phone': '+79218418889', 'token': 'HxrQxl-1uxnBSjEhzP8jnw'},
-    # {'phone': '+79517288206', 'token': 'X9izNzavL2GFFzBAKa8qWw'},
-    # {'phone': '+79517228979', 'token': 'Wg7Vy7EHeF1N7-gXw6VPWQ'},
-    # {'phone': '+79517218799', 'token': 'Ee4g4azltSfZOS1m2cYvgg'},
-    # {'phone': '+79539052801', 'token': 'cO5gOtS50iqV6OjUkvibtw'},
-    # {'phone': '+79021489271', 'token': 'L9LaabVH6OMrbzAA3xUwcw'},
-    # {'phone': '+79602018075', 'token': 'CCnw_rT7BdBwThrDyVRAbQ'},
-    # {'phone': '+79524876078', 'token': 'CFpsegCJ2pvwuCQlzBGGtQ'},
-    # {'phone': '+79809963188', 'token': 'TG9ztGkNPET91vYRuQjnMw'},
-    # {'phone': '+79524806163', 'token': 'ocjot8-hVyxq-0PUosb-Kw'},
-    # {'phone': '+79524807855', 'token': 'osoXgIvw-oYQiqm5LHgASQ'},
-    # {'phone': '+79517228978', 'token': 'be79GyGGxhVs8g-1JksP-Q'},
-    # {'phone': '+79539058892', 'token': 'K0biieWo8ffAshmMIP447w'},
+    {'phone': '+79517287194', 'token': '6Hnojq9_fwS-yXaiawJ1og'},
+    {'phone': '+79218418889', 'token': 'HxrQxl-1uxnBSjEhzP8jnw'},
+    {'phone': '+79517288206', 'token': 'X9izNzavL2GFFzBAKa8qWw'},
+    {'phone': '+79517228979', 'token': 'Wg7Vy7EHeF1N7-gXw6VPWQ'},
+    {'phone': '+79517218799', 'token': 'Ee4g4azltSfZOS1m2cYvgg'},
+    {'phone': '+79539052801', 'token': 'cO5gOtS50iqV6OjUkvibtw'},
+    {'phone': '+79021489271', 'token': 'L9LaabVH6OMrbzAA3xUwcw'},
+    {'phone': '+79602018075', 'token': 'CCnw_rT7BdBwThrDyVRAbQ'},
+    {'phone': '+79524876078', 'token': 'CFpsegCJ2pvwuCQlzBGGtQ'},
+    {'phone': '+79809963188', 'token': 'TG9ztGkNPET91vYRuQjnMw'},
+    {'phone': '+79524806163', 'token': 'ocjot8-hVyxq-0PUosb-Kw'},
+    {'phone': '+79524807855', 'token': 'osoXgIvw-oYQiqm5LHgASQ'},
+    {'phone': '+79517228978', 'token': 'be79GyGGxhVs8g-1JksP-Q'},
+    {'phone': '+79539058892', 'token': 'K0biieWo8ffAshmMIP447w'},
 
     # {'phone': '+79150187307', 'token': 'W3voQDYWS9LmHLP7Xz_eOg'},
     # {'phone': '+79163465407', 'token': 'W3voQDYWS9LmHLP7Xz_eOg'},
     # {'phone': '+79251286565', 'token': 'pMUFw-UXN6E-P1yHG7NcwA'},
-    {'phone': '+79163465407', 'token': 'pMUFw-UXN6E-P1yHG7NcwA'},
+    # {'phone': '+79636936003', 'token': 'pMUFw-UXN6E-P1yHG7NcwA'},
 ]
 
 logging.basicConfig(
